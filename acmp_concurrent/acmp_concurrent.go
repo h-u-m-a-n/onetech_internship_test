d32a775ac29a8a7e5ea132dc1e422b3f28385a25
Sanzhar <190103205@stu.sdu.edu.kz>
2021-11-13 18:17:45 +0600

    acmp_concurrent imported func from acmp instead of copying func

--- acmp_concurrent/acmp_concurrent.go
+++ acmp_concurrent/acmp_concurrent.go
@@ -1,12 +1,7 @@
 package acmp_concurrent
 
 import (
-	"io"
-	"log"
-	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
+	"github.com/h-u-m-a-n/onetech_internship_test/acmp"
 	"sync"
 )
 
@@ -20,7 +15,7 @@ func Difficulties(urls []string) map[string]float64 {
 		wg.Add(1)
 		go func(url string) {
 			defer wg.Done()
-			res := Difficulty(url)
+			res := acmp.Difficulty(url)
 			mu.Lock()
 			defer mu.Unlock()
 			m[url] = res
@@ -28,49 +23,4 @@ func Difficulties(urls []string) map[string]float64 {
 	}
 	wg.Wait()
 	return m
-}
-
-func Difficulty(url string) float64 {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Printf("couldn't make requst: %v", err)
-		return -1
-	}
-	req.AddCookie(&http.Cookie{
-		Name:  "English",
-		Value: "1",
-	})
-	response, err := client.Do(req)
-	if err != nil {
-		log.Printf("couldn't make requst2: %v", err)
-		return -1
-	}
-	defer response.Body.Close()
-	if response.StatusCode != http.StatusOK {
-		return -1
-	}
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Printf("couldn't read body: %v", err)
-		return -1
-	}
-	reg, err := regexp.Compile(`Difficulty: (\d+)%`)
-	if err != nil {
-		log.Printf("error while compiling reg exp: %v", err)
-		return -1
-	}
-	difficulty := string(reg.Find(body))
-	if len(difficulty) == 0 {
-		log.Printf("couldn't find difficulty: %v", err)
-		return -1
-	}
-	resStr := strings.Split(difficulty, " ")[1]
-	resStr = resStr[:len(resStr)-1]
-	res, err := strconv.ParseFloat(resStr, 64)
-	if err != nil {
-		log.Printf("couldn't parse difficulty: %v", err)
-		return -1
-	}
-	return res
 }
